8ece7f8c13145736330ad3709df4fbf56c0e7dd4
BruceAko <dczdcz2001@aliyun.com>
2023-01-15 17:02:10 +0800

    finish simpleDict

--- simpledict/homework/main.go
+++ simpledict/homework/main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
+	"sync"
+	"time"
 )
+
+var wg sync.WaitGroup
 
 type DictRequest struct {
 	TransType string `json:"trans_type"`
 	Source    string `json:"source"`
 	UserID    string `json:"user_id"`
+}
+
+type DictRequest2 struct {
+	Q     string
+	From  string
+	To    string
+	Appid int
+	Salt  int
+	Sign  string
 }
 
 type DictResponse struct {
@@ -47,6 +64,12 @@ type DictResponse struct {
 		Related      []interface{} `json:"related"`
 		Source       string        `json:"source"`
 	} `json:"dictionary"`
+}
+
+type DictResponse2 struct {
+	From        string              `json:"from"`
+	To          string              `json:"to"`
+	TransResult []map[string]string `json:"trans_result"`
 }
 
 func query(word string) {
@@ -84,7 +107,7 @@ func query(word string) {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	bodyText, err := ioutil.ReadAll(resp.Body)
+	bodyText, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -96,19 +119,81 @@ func query(word string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("彩云翻译结果：")
 	fmt.Println(word, "UK:", dictResponse.Dictionary.Prons.En, "US:", dictResponse.Dictionary.Prons.EnUs)
 	for _, item := range dictResponse.Dictionary.Explanations {
 		fmt.Println(item)
 	}
+	wg.Done()
+}
+
+func query2(word string) {
+	var appID = 20210225000707349
+	var password = "rXOf9TWDrZSTrqdmUH4r"
+	tran := DictRequest2{
+		Q:     word,
+		From:  "en",
+		To:    "cht",
+		Appid: appID,
+	}
+	tran.Salt = time.Now().Second()
+	content := strconv.Itoa(appID) + word + strconv.Itoa(tran.Salt) + password
+	sign := SumString(content) //计算sign值
+	tran.Sign = sign
+	values := tran.ToValues()
+	resp, err := http.PostForm("https://api.fanyi.baidu.com/api/trans/vip/translate", values)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("bad StatusCode:", resp.StatusCode, "body", string(body))
+	}
+	var dictResponse DictResponse2
+	err = json.Unmarshal(body, &dictResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("百度翻译结果：")
+	for _, m := range dictResponse.TransResult {
+		fmt.Println(m["dst"])
+	}
+	wg.Done()
+}
+
+func SumString(content string) string {
+	md5Ctx := md5.New()
+	md5Ctx.Write([]byte(content))
+	bys := md5Ctx.Sum(nil)
+	//bys := md5.Sum([]byte(content))//这个md5.Sum返回的是数组,不是切片哦
+	value := hex.EncodeToString(bys)
+	return value
+}
+
+func (tran DictRequest2) ToValues() url.Values {
+	values := url.Values{
+		"q":     {tran.Q},
+		"from":  {tran.From},
+		"to":    {tran.To},
+		"appid": {strconv.Itoa(tran.Appid)},
+		"salt":  {strconv.Itoa(tran.Salt)},
+		"sign":  {tran.Sign},
+	}
+	return values
 }
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, `usage: simpleDict WORD
-example: simpleDict hello
-		`)
+		fmt.Fprintf(os.Stderr, "usage: simpleDict WORD\nexample: simpleDict hello\n")
 		os.Exit(1)
 	}
 	word := os.Args[1]
-	query(word)
+	wg.Add(2)
+	go query(word)
+	go query2(word)
+	wg.Wait()
 }
